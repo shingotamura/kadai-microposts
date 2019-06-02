fc0221decb1e393a98810f0a3deecb27a55035a1
shingo.tamura <mr.shingo-0409@outlook.jp>
2019-06-02 06:45:45 +0000

    post

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,36 @@
+class MicropostsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :current_user,only:[:destroy]
+  
+  def create
+    @micropost=current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:succes]="メッセージを投稿しました"
+      redirect_to root_url
+    else
+      flash.now[:denger]="メッセージの投稿に失敗しました"
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    flash[:succes]="メッセージを削除しました"
+    redirect_back(fallback_location:root_path)
+  end
+
+
+private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+  
+  def current_user
+    @micropost=current_user.microposts.find_by(id:params[:id])
+    unless @micropost
+    redirect_to root_url
+      
+    end
+      
+    end
+end
